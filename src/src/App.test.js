6b4ddbe8e775e8d6a0dfa2a3aef899c38c489b08
mishacarlos55 <mishacarlos55@used_on_github.com>
2026-04-15 21:21:04 +0530

    fix app tests

--- src/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders student task manager heading", () => {
-  render(<App />);
-  const heading = screen.getByText(/student task manager/i);
-  expect(heading).toBeInTheDocument();
-});
